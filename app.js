a668014d57d11389e8df046126f58b778e646dc2
abhaya kumar sahoo <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2022-10-10 14:13:52 +0530

    chnaged

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
-// const loginRoute=require('./src/router/LoginRegistrationApi/LoginRegistration');
-// const AddProject=require('./src/router/AddProject/AddProject');
-// const Reports = require("./src/router/Reports/Reports");
-// const Posts = require("./src/router/Post/Post");
-// const MyProjects = require("./src/router/MyProjects/MyProjects");
+const loginRoute=require('./src/router/LoginRegistrationApi/LoginRegistration');
+const AddProject=require('./src/router/AddProject/AddProject');
+const Reports = require("./src/router/Reports/Reports");
+const Posts = require("./src/router/Post/Post");
+const MyProjects = require("./src/router/MyProjects/MyProjects");
 
-// dotenv.config();
+dotenv.config();
 
-// const PORT = process.env.PORT;
-// app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.json());
+const PORT = process.env.PORT;
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
 
-// //Database Connection
-// require("./src/db/connections");
+//Database Connection
+require("./src/db/connections");
 
-// //Routes
-// app.use('',loginRoute)
+//Routes
+app.use('',loginRoute)
 
-// app.use('',AddProject)
+app.use('',AddProject)
 
-// app.use('',MyProjects)
+app.use('',MyProjects)
 
-// app.use('',Reports)
+app.use('',Reports)
 
-// app.use('',Posts)
+app.use('',Posts)
 
 
 app.get("/", (req, res) => {
