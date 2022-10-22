92c5d8bc5b9952e271378188cdb00baf37461fb1
abhaya kumar sahoo <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2022-10-22 08:51:13 +0530

    fs

--- src/router/Reports/Reports.js
+++ src/router/Reports/Reports.js
@@ -2,14 +2,20 @@ const express = require("express");
 const auth = require("../../middleware/authenticate");
 const router = express.Router();
 const AddProjectSchema = require("../../schema/projectSchema");
-
+const PostSchema = require("../../schema/postSchema");
 router.post("/project_dates", auth, async (req, res) => {
-  const data = await AddProjectSchema.find({ postedBy: req.user._id }).select({
-    created_date: 1,
-    created_time: 1,
-  });
+  const { project_id } = req.body;
 
-  return res.send({ msg: "Successful", data });
+  const data = await PostSchema.find({ project_id })
+    .select({
+      "project_comments.created_date": 1,
+    })
+    .distinct("project_comments.created_date");
+  // .select({
+  //   created_date: 1,
+  // })
+
+  return res.send({ msg: "Successful", data, error: false });
 });
 
 router.post("/project_details", auth, async (req, res) => {
