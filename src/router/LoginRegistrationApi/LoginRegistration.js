0d35a25adbf5d357965dd2a75c813794a2b87979
abhaya kumar sahoo <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2022-10-27 20:15:40 +0530

    search

--- src/router/LoginRegistrationApi/LoginRegistration.js
+++ src/router/LoginRegistrationApi/LoginRegistration.js
@@ -134,7 +134,15 @@ router.post("/all_members", auth, async (req, res) => {
 
 router.post("/all_users", auth, async (req, res) => {
   try {
-    const data = await User.find({ _id: { $ne: req.user._id } }).select({
+    const { text } = req.body;
+    const test = text.replace('"', "/");
+
+    const data = await User.find({
+      $and: [
+        { _id: { $ne: req.user._id } },
+        { name: { $regex: test, $options: "i" } },
+      ],
+    }).select({
       name: 1,
       selected: 1,
       isAdmin: 1,
